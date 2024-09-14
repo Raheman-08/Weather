7d9d9c74ac4f18f0f6ffb41b899b9ff59b173751
Raheman Ali <49222155+Raheman-08@users.noreply.github.com>
2024-09-14 11:27:53 +0530

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Raheman Ali on 14/09/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
